fb6df9d80e53faecbfcfefe3622515f1c4931632
Kevin Coppa <khc339@nyu.edu>
2018-02-19 23:05:16 -0500

    CreateBookings table

--- db/migrate/20180210213438_create_bookings.rb
+++ db/migrate/20180210213438_create_bookings.rb
@@ -1,6 +1,10 @@
 class CreateBookings < ActiveRecord::Migration[5.1]
   def change
     create_table :bookings do |t|
+      t.integer :booker_id, null: false
+      t.integer :date, null: false
+      t.date :start_time, null: false
+      t.date :end_time, null: false
 
       t.timestamps
     end
