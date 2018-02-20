47f23db64f1ab031bdbb4fe30e102eda1d4e1a48
Kevin Coppa <khc339@nyu.edu>
2018-02-19 23:05:43 -0500

    Write booking model validations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,5 @@
 class Booking < ApplicationRecord
-  
+  validates :booker_id, :date, :start_time, :end_time presence: true
+  validates_uniqueness_of :date, :scope => [:start_time, :end_time]
 
 end
