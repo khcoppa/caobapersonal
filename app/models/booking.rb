6bea82b98d4cde92fe983b79d60aa805bef2a843
Kevin Coppa <khc339@nyu.edu>
2018-02-20 21:16:02 -0500

    Validates for booking model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,5 +1,6 @@
 class Booking < ApplicationRecord
   validates :booker_id, :date, :start_time, :end_time presence: true
-  validates_uniqueness_of :date, :scope => [:start_time, :end_time]
+  validates_uniqueness_of :date, :scope => [:start_time, ]
+
 
 end
--- db/migrate/20180210213438_create_bookings.rb
+++ db/migrate/20180210213438_create_bookings.rb
@@ -2,9 +2,9 @@ class CreateBookings < ActiveRecord::Migration[5.1]
   def change
     create_table :bookings do |t|
       t.integer :booker_id, null: false
-      t.integer :date, null: false
-      t.date :start_time, null: false
-      t.date :end_time, null: false
+      t.date :date, null: false
+      t.datetime :start_time, null: false
+      t.datetime :end_time, null: false
 
       t.timestamps
     end
