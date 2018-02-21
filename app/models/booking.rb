30e7dfae4beb41587ba11ec1d09ac67b01412ea8
Kevin Coppa <khc339@nyu.edu>
2018-02-20 21:18:40 -0500

    Active Record assocations for Booking model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,9 @@
 class Booking < ApplicationRecord
-  validates :booker_id, :date, :start_time, :end_time presence: true
-  validates_uniqueness_of :date, :scope => [:start_time, ]
+  validates :booker_id, :date, :start_time, :end_time, presence: true
+  validates_uniqueness_of :date, :scope => [:start_time, :end_time]
 
+  belongs_to :booker,
+    foreign_key: :booker_id,
+    class_name: 'User'
 
 end
