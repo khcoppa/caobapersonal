397302e2002d13386c1992161b57e5c997cb3dcb
Kevin Coppa <khc339@nyu.edu>
2018-03-12 23:45:13 -0400

    Start styling in vertical sections

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180125231951) do
+ActiveRecord::Schema.define(version: 20180210213438) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.integer "booker_id", null: false
+    t.date "date", null: false
+    t.datetime "start_time", null: false
+    t.datetime "end_time", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
