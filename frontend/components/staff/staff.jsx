83a5673fc83491c15ea99e593b4e220f12e5902c
Kevin Coppa <khc339@nyu.edu>
2018-03-17 15:53:00 -0400

    Staff.jsx component content

---
+++ frontend/components/staff/staff.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+class Staff extends React.Component {
+  render() {
+    return (
+      <div className="staff">
+
+        <div className="staff-header">
+          <h3>CAOBA Personal Staff</h3>
+        </div>
+
+        <div className="master">
+          <h4>Irani Andreati</h4>
+          <p>
+          Master Bio
+          </p>
+
+          <div className="master-img">
+          </div>
+        </div>
+
+      </div>
+    );
+  }
+}
+
+export default Home;
