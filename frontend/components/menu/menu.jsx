ef067fd9cb69977cadae7bd4dede17de8a717111
Kevin Coppa <khc339@nyu.edu>
2018-03-18 18:54:47 -0400

    Menu Link to tags update

--- frontend/components/menu/menu.jsx
+++ frontend/components/menu/menu.jsx
@@ -5,17 +5,20 @@ class Menu extends React.Component {
   render() {
     return (
       <div className="menu">
+
         <ul className="menu_public">
-          <li><a href="/">Training</a></li>
-          <li><a href="/">About</a></li>
-          <li><a href="#/staff">Meet Master</a></li>
-          <li><a href="/">Caoba</a></li>
+          <li><Link to={'/training'}>Training</Link></li>
+          <li><Link to={'/about'}>About</Link></li>
+          <li><Link to={'/staff'}>Mestre</Link></li>
+          <li><Link to={'/caoba'}>Caoba</Link></li>
         </ul>
+
         <ul className = "menu_protected">
-          <li><a href="/">Join</a></li>
-          <li><a href="#/login">Login</a></li>
-          <li><a href="/">Book Session</a></li>
+          <li><Link to={'/join'}>Join</Link></li>
+          <li><Link to={'/login'}>Login</Link></li>
+          <li><Link to={'/book'}>Book Session</Link></li>
         </ul>
+        
       </div>
     );
   }
